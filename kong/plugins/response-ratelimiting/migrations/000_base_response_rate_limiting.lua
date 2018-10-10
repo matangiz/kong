bdc7fb90d0c2e8094da59fe9b905813bc8fa0eed
Hisham Muhammad <hisham@gobolinux.org>
2018-10-09 21:12:20 -0300

    fix(response-ratelimiting) new increment operation for Postgres 9.5+

--- kong/plugins/response-ratelimiting/migrations/000_base_response_rate_limiting.lua
+++ kong/plugins/response-ratelimiting/migrations/000_base_response_rate_limiting.lua
@@ -13,33 +13,6 @@ return {
         PRIMARY KEY ("identifier", "period", "period_date", "service_id", "route_id", "api_id")
       );
 
-      CREATE OR REPLACE FUNCTION "increment_response_rate_limits" (a_id UUID, i TEXT, p TEXT, p_date TIMESTAMP WITH TIME ZONE, v INTEGER) RETURNS void
-      LANGUAGE plpgsql
-      AS $$
-        BEGIN
-          LOOP
-            UPDATE response_ratelimiting_metrics
-               SET value = value + v
-             WHERE identifier = i
-               AND period = p
-               AND period_date = p_date
-               AND api_id = a_id;
-
-            IF FOUND THEN
-              RETURN;
-            END IF;
-
-            BEGIN
-              INSERT INTO response_ratelimiting_metrics (identifier, period, period_date, api_id, value)
-                   VALUES (i, p, p_date, a_id, v);
-              RETURN;
-            EXCEPTION WHEN unique_violation THEN
-
-            END;
-          END LOOP;
-        END;
-        $$;
-
       CREATE OR REPLACE FUNCTION increment_response_rate_limits (r_id UUID, s_id UUID, i TEXT, p TEXT, p_date TIMESTAMP WITH TIME ZONE, v INTEGER) RETURNS void
       LANGUAGE plpgsql
       AS $$
--- kong/plugins/response-ratelimiting/migrations/001_14_to_15.lua
+++ kong/plugins/response-ratelimiting/migrations/001_14_to_15.lua
@@ -3,6 +3,30 @@ return {
     up = [[
       ALTER TABLE IF EXISTS ONLY "response_ratelimiting_metrics"
         ALTER "period_date" TYPE TIMESTAMP WITH TIME ZONE USING "period_date" AT TIME ZONE 'UTC';
+
+      CREATE OR REPLACE FUNCTION increment_response_rate_limits (r_id UUID, s_id UUID, i TEXT, p TEXT, p_date TIMESTAMP WITH TIME ZONE, v INTEGER) RETURNS void
+      LANGUAGE plpgsql
+      AS $$
+        BEGIN
+          INSERT INTO response_ratelimiting_metrics AS old(identifier, period, period_date, service_id, route_id, value)
+               VALUES (i, p, p_date, s_id, r_id, v)
+          ON CONFLICT ON CONSTRAINT response_ratelimiting_metrics_pkey
+          DO UPDATE SET value = old.value + v;
+
+        END;
+        $$;
+
+      CREATE OR REPLACE FUNCTION increment_response_rate_limits_api (a_id UUID, i TEXT, p TEXT, p_date TIMESTAMP WITH TIME ZONE, v INTEGER) RETURNS void
+      LANGUAGE plpgsql
+      AS $$
+        BEGIN
+          INSERT INTO response_ratelimiting_metrics AS old(identifier, period, period_date, api_id, value)
+               VALUES (i, p, p_date, a_id, v)
+          ON CONFLICT ON CONSTRAINT response_ratelimiting_metrics_pkey
+          DO UPDATE SET value = old.value + v;
+        END;
+        $$;
+
     ]],
   },
 
